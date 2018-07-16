abbbdb544d8181161e029cfd7de717ee1880da79
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-16 11:08:57 -0600

    Create post item endpoint

--- server.js
+++ server.js
@@ -15,3 +15,23 @@ app.use(express.static('public'))
 app.listen(app.get('port'), () => { 
   console.log(`${app.locals.title} is running on ${app.get('port')}.`) 
 })
+
+app.post('/api/v1/items', (request, response) => {
+  const { item } = request.body;
+
+  for(let requiredParameter of ['name', 'packed']) {
+    if (!item[requiredParameter]){
+      return response
+        .status(422)
+        .send({ error: `Expected format: { item: <String>, packed: <Boolean> }. You're missing a "${requiredParameter}" property.` })
+    }
+  }
+
+  database('items').insert(item, 'id')
+    .then(item => {
+      response.status(201).json({id: item[0]})
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+})
