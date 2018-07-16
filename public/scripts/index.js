b917c60f1802ccb3066a1056d6d372d514ac616a
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-16 10:16:36 -0600

    Add basic styles to card template

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,12 @@
-console.log('woo')
+const submitButton = document.querySelector('.submit-button');
+const userInput = document.querySelector('.input-text');
+const itemSection = document.querySelector('.item-section');
+
+submitButton.addEventListener('click', addItem);
+
+
+
+function addItem(event) {
+  event.preventDefault();
+  itemSection.innerHTML = userInput.value;
+}
