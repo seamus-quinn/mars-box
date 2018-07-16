82905e0bb1fd521cb2c43056e79ca560f35aef92
Seamus Quinn <sumaesquinn@gmail.com>
2018-07-16 10:24:33 -0600

    Add card creation functionality

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,5 +8,18 @@ submitButton.addEventListener('click', addItem);
 
 function addItem(event) {
   event.preventDefault();
-  itemSection.innerHTML = userInput.value;
+  const card = createCard(userInput.value);
+  console.log(card);
+  itemSection.innerHTML = card;
+}
+
+function createCard(userInput) {
+  return `<div class="card">
+      <h1 class="card-name">${userInput}</h1>
+      <form class="checkbox-form">
+        <input class="checkbox" type="checkbox" />
+        <label for="checkbox">Packed</label>
+      </form>
+      <button class="delete-button">Delete</button>
+    </div>`
 }
